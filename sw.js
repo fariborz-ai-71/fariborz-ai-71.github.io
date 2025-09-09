faeb39a5bbdc283be8646f37e8c32bfb6bb44264
Fariborz Ekhtiari <fariborze7300@gmail.com>
2025-09-09 23:50:19 +0330

    Update sw.js
    Remove service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
 
-const CACHE = "fo-portfolio-v1";
-const ASSETS = ["./","./index.html","./styles.css","./script.js","./assets/projects.json","./assets/favicon.svg","./assets/resume.pdf"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
